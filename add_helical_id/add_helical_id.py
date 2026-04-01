ae638af87719d71694b879a02e9f887401e7b0fb
Sander Roet <sanderroet@hotmail.com>
2026-04-01 13:56:43 +0200

    also add subset splits

--- add_helical_id/add_helical_id.py
+++ add_helical_id/add_helical_id.py
@@ -16,12 +16,20 @@ def main(input_file: str, output_file: str, cutoff: float):
     # quickly produce distances between the particle and the next
     distances = np.sum((array - np.roll(array, -1, axis=0)) ** 2, axis=1) ** 0.5
     val = 1
+    # need to randomize initial subset as this runs for 1 tomo only
+    subset = np.random.choice([1,2])
+    # don't keep randomizing subsets as we assume this has less drift from the wanted 50/50
+    new_subset = {1: 2, 2: 1}
+    subsets = []
     values = []
     for dist in distances:
         values.append(val)
+        subsets.append(subset)
         if dist >= cutoff:
             val += 1
+            subset = new_subset[subset]
     data["rlnHelicalTubeID"] = values
+    data["rlnRandomSubset"] = subsets
     starfile.write(data, output_file)
 
 
